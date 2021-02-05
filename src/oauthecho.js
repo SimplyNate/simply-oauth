d360733b330158e30622acea629af9bdfe61b1ec
SimplyNate <horiuchi.nathan@gmail.com>
2021-02-04 19:01:28 -1000

    Changed authorize_callback from null to empty string to stop IDE from throwing warning

--- src/oauthecho.js
+++ src/oauthecho.js
@@ -3,7 +3,7 @@ const OAuth = require('./oauth');
 class OAuthEcho extends OAuth {
 
     constructor(realm, verify_credentials, consumerKey, consumerSecret, version, signatureMethod, nonceSize, customHeaders) {
-        super(null, null, consumerKey, consumerSecret, version, null, signatureMethod, nonceSize, customHeaders);
+        super(null, null, consumerKey, consumerSecret, version, '', signatureMethod, nonceSize, customHeaders);
         this._isEcho = true;
         this._realm = realm;
         this._verifyCredentials = verify_credentials;
